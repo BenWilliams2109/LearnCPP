8e1920ad7dfefb2095e89776279b6c93f49ea2ce
Ben Williams <benwilliams.business@outlook.com>
2021-04-16 15:32:51 +0100

    Add project files.

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int inputInteger{};
+
+	cout << "Enter an integer: ";
+	cin >> inputInteger;
+	cout << "Double that number is: " << inputInteger * 2 << endl;
+
+	return 0;
+}
